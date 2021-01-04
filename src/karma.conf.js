e573f081798dbaf63d5162ad98b628eec2ef7492
LennyDennis <lennydennis@gmail.com>
2021-01-04 15:14:43 +0300

    Add image files'

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/Kamsos-Web'),
+      dir: require('path').join(__dirname, '../coverage/Baricho-High-Web'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
